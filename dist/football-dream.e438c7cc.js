932359dfd1a0b59d48f3ff573d1d5b78774214e7
Marko Tintor <marko.tintor.18@singimail.rs>
2025-03-24 20:37:06 +0100

    testing

--- dist/football-dream.e438c7cc.js
+++ dist/football-dream.e438c7cc.js
@@ -662,14 +662,10 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"cCMpN":[function(require,module,exports,__globalThis) {
+const token = "	cc13e9364e52402183503e253e324864";
 const showScorers = async function() {
     try {
-        const res = await fetch("https://api.football-data.org/v4/areas/", {
-            method: "GET",
-            headers: {
-                "X-Auth-Token": "cc13e9364e52402183503e253e324864"
-            }
-        });
+        const res = await fetch(" https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Everton");
         if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
         const data = await res.json();
         console.log(data);
