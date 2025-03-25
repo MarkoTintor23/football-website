21e7aca6f365a16e4c18e2d0cf6e15f283a444be
Marko Tintor <marko.tintor.18@singimail.rs>
2025-03-25 22:18:55 +0100

    rendering api

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,8 @@
-const token = "	cc13e9364e52402183503e253e324864";
-
+const sideContainer = document.querySelector(".sidebar");
 const showTeam = async function () {
   try {
     const res = await fetch(
-      " https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Liverpool"
+      " https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Arsenal"
     );
     const data = await res.json();
 
@@ -25,6 +24,50 @@ const showTeam = async function () {
     };
 
     console.log(team);
+
+    const markup = `
+    <figure class="team__fig">
+          <img src=${team.banner} alt=${team.club} class="team__img" />
+          <h1 class="team__title">
+            <span>${team.club}</span>
+          </h1>
+        </figure>
+
+        <div class="team__details">
+          <div class="team__info">
+            <svg class="team__info-icon">
+              <use href=${team.logo}></use>
+            </svg>
+           
+            <span class="team__info-text">${team.description}</span>
+          </div>
+
+        <div class="team__additionals">
+          <h2 class="heading--2">${team.country}</h2>
+          <ul class="team__additional-list">
+            <li class="team__additional">
+              <div class="team__quantity">${team.stadiumCapacity}</div>
+              <div class="team__description">
+                <span class="team__unit">${team.stadium}</span>
+           
+              </div>
+            </li>
+            <li class="team__additional">
+              <div class="team__quantity">${team.location}</div>
+              <div class="team__description">
+                <span class="team__unit">${team.formedYear}</span>
+             
+              </div>
+            </li>
+        <div class="team__directions">
+          <h2 class="heading--2">${team.league}</h2>
+          <p class="team__directions-text">
+            This team was carefully designed and tested by
+            <span class="team__publisher">The Pioneer Woman</span>. Please check out
+            directions at their website.
+          </p>
+        </div>`;
+    sideContainer.insertAdjacentHTML("afterbegin", markup);
   } catch (err) {
     alert("Error: " + err.message);
   }
