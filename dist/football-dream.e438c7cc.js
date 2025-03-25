db35026770f98f7a451d9266cf2c35cdc36c4f1f
Marko Tintor <marko.tintor.18@singimail.rs>
2025-03-25 19:45:31 +0100

    api fetch

--- dist/football-dream.e438c7cc.js
+++ dist/football-dream.e438c7cc.js
@@ -663,17 +663,30 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"cCMpN":[function(require,module,exports,__globalThis) {
 const token = "	cc13e9364e52402183503e253e324864";
-const showScorers = async function() {
+const showTeam = async function() {
     try {
-        const res = await fetch(" https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Everton");
-        if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+        const res = await fetch(" https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Liverpool");
         const data = await res.json();
-        console.log(data);
+        if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+        let team = data.teams[0];
+        team = {
+            club: team.strTeam,
+            description: team.strDescriptionEN,
+            country: team.strCountry,
+            league: team.strLeague,
+            location: team.strLocation,
+            stadium: team.strStadium,
+            stadiumCapacity: team.intStadiumCapacity,
+            formedYear: team.intFormedYear,
+            logo: team.strBadge,
+            banner: team.strBanner
+        };
+        console.log(team);
     } catch (err) {
         alert("Error: " + err.message);
     }
 };
-showScorers();
+showTeam();
 
 },{}]},["67rsS","cCMpN"], "cCMpN", "parcelRequire4472")
 
--- js/controller.js
+++ js/controller.js
@@ -1,17 +1,32 @@
 const token = "	cc13e9364e52402183503e253e324864";
 
-const showScorers = async function () {
+const showTeam = async function () {
   try {
     const res = await fetch(
-      " https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Everton"
+      " https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Liverpool"
     );
+    const data = await res.json();
 
     if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
 
-    const data = await res.json();
-    console.log(data);
+    let team = data.teams[0];
+
+    team = {
+      club: team.strTeam,
+      description: team.strDescriptionEN,
+      country: team.strCountry,
+      league: team.strLeague,
+      location: team.strLocation,
+      stadium: team.strStadium,
+      stadiumCapacity: team.intStadiumCapacity,
+      formedYear: team.intFormedYear,
+      logo: team.strBadge,
+      banner: team.strBanner,
+    };
+
+    console.log(team);
   } catch (err) {
     alert("Error: " + err.message);
   }
 };
-showScorers();
+showTeam();
